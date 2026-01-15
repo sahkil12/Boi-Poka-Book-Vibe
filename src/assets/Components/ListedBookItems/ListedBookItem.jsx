f43ba3425514934bb8bed5c01829d398dc08c3c8
Tazwer Shakil <tazwershakilshakil@gmail.com>
2026-01-15 21:57:00 +0600

    update text size and gapo

--- src/assets/Components/ListedBookItems/ListedBookItem.jsx
+++ src/assets/Components/ListedBookItems/ListedBookItem.jsx
@@ -39,7 +39,7 @@ const ListedBookItem = () => {
         </Helmet>
       {/* heading text part */}
       <div className="text-center work-sans">
-        <h2 className="text-4xl font-bold my-10">Books</h2>
+        <h2 className="text-4xl md:text-5xl font-bold my-10">Books</h2>
 {/* sort by */}
         <div className="dropdown dropdown-center  my-10 mb-15">
   <div tabIndex={0} role="button" className="btn w-60 flex justify-between bg-green-500 text-white text-lg">Sort By<span>⬇</span></div>
@@ -60,7 +60,7 @@ const ListedBookItem = () => {
         <TabPanel>
           {/* read list tabs */}
           <div className="my-10">
-            {readList.length === 0 && <h2 className="md:w-2/5 mx-auto text-4xl/normal md:text-6xl/normal font-bold text-center my-40">You haven’t added any books to your Read list yet.</h2>}
+            {readList.length === 0 && <h2 className="md:w-2/5 mx-auto text-4xl/normal font-bold text-center my-40">You haven’t added any books to your Read list yet.</h2>}
             {readList?.map((book) => (
               <ReadBookItem key={book.bookId} book={book}></ReadBookItem>
             ))}
@@ -70,7 +70,7 @@ const ListedBookItem = () => {
           {/* wish list tab */}
         <div className="my-10">
           {
-            wishList.length === 0 && <h2 className="md:w-2/5 mx-auto text-4xl/normal md:text-6xl/normal font-bold text-center my-40">You haven’t added any books to your Wish list yet.</h2>
+            wishList.length === 0 && <h2 className="md:w-2/5 mx-auto text-4xl/normal font-bold text-center my-40">You haven’t added any books to your Wish list yet.</h2>
           }
           {
             wishList?.map((book)=>
